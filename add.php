f3f2ce4c21464a10be85f9951fd7ec40c58c8c6d
GuillaumeLoriot <loriot.guillaume@gmail.com>
2025-03-07 16:54:20 +0100

    commit end of day

---
+++ add.php
@@ -0,0 +1,63 @@
+<?php
+require_once("header.php");
+require_once("connectDB.php");
+$errors=[];
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    if(empty($_POST['model'])){
+        $errors['model']='ce champ ne peut pas être vide';
+    }
+    if(empty($_POST['brand'])){
+        $errors['brand']='ce champ ne peut pas être vide';
+    }
+    if(empty($_POST['horsePower'])){
+        $errors['horsePower']='ce champ ne peut pas être vide';
+    }
+    if(empty($_POST['image'])){
+        $errors['image']='ce champ ne peut pas être vide';
+    }
+    ?>
+<?php
+    
+}
+?>
+<form method="POST" action="add.php">
+<label for="model">Modèle</label>
+<input type="text" name="model" placeholder="EX: Eldorado">
+<?php if(isset($errors['model'])){
+?>
+<p class="errors"><?php echo($errors['model']); ?></p>
+<?php
+}
+?>
+<label for="brand">Marque</label>
+<input type="text" name="brand" placeholder="EX: Cadillac">
+<?php if(isset($errors['brand'])){
+?>
+<p class="errors"><?php echo($errors['brand']); ?></p>
+<?php
+}
+?>
+<label for="horsePower">nombre de chevaux</label>
+<input type="number" name="horsePower" placeholder="EX: 210">
+<?php if(isset($errors['horsePower'])){
+?>
+<p class="errors"><?php echo($errors['horsePower']); ?></p>
+<?php
+}
+?>
+<label for="image">nom du fichier image de la voiture</label>
+<input type="text" name="image" placeholder="EX: Eldorado.jpg">
+<?php if(isset($errors['image'])){
+?>
+<p class="errors"><?php echo($errors['image']); ?></p>
+<?php
+}
+?>
+<input type="submit" value="valider">
+</form>
+
+
+<?php
+require_once("footer.php");
+?>
--- index.php
+++ index.php
@@ -1,19 +1,34 @@
 <?php
-require_once ('header.php');
-require_once ('connectDB.php');
+require_once('header.php');
+require_once('connectDB.php');
 
 $pdo = connectDB();
 
 $requete = $pdo->query("SELECT * FROM car;");
 $cars = $requete->fetchAll();
-foreach($cars as $car){
-    ?>
-<p><?php echo($car['id'] . $car['model'] . $car['brand'] . $car['horsePower']);?></p>
-<img src="images/<?php echo($car['image']);?>" alt="">
+?>
+<a href="add.php" class="add">Ajouter un véhicule</a>
+<section>
+<?php
+foreach ($cars as $car) {
+?>
+
+    <Div class="card">
+        <img src="images/<?php echo ($car['image']); ?>" alt="">
+        <div class="info">
+            <h2><?php echo ($car['model']); ?></h2>
+            <p><?php echo ($car['brand'] . ",  " . $car['horsePower'] . " chevaux"); ?></p>
+        </div>
+        <div class="card_buttons">
+            <a href="update.php?=<?php echo($car["id"]); ?>" class="update">Modifier</a>
+            <a href="delete.php?=<?php echo($car["id"]); ?>" class="delete">Supprimer</a>
+        </div>
+    </Div>
+
 <?php
 }
-
-
-
-require_once ('footer.php');
+?>
+</section>
+<?php
+require_once('footer.php');
 ?>
